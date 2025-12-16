e59dd8795f21635e36992dff99deabb6019d086f
s.butvin <serhii.bu@kraken-leads.com>
2025-12-16 23:35:55 +0200

    update #5

--- src/Domain/GoogleApi/ValueObject/DomainValueObject.php
+++ src/Domain/GoogleApi/ValueObject/DomainValueObject.php
@@ -4,15 +4,29 @@ declare(strict_types=1);
 
 namespace Domain\GoogleApi\ValueObject;
 
+use InvalidArgumentException;
+use DomainException;
+
 readonly class DomainValueObject
 {
+    private const int DOMAIN_NAME_LENGTH = 3;
     private function __construct(private string $value) {}
 
     public static function fromString(string $value): self
     {
         $value = mb_strtolower(trim($value));
-        if (!filter_var($value, FILTER_VALIDATE_DOMAIN, FILTER_FLAG_HOSTNAME) && !preg_match('/^[a-z0-9.-]+$/', $value)) {
-            throw new \InvalidArgumentException(
+
+        if (strlen($value) < self::DOMAIN_NAME_LENGTH) {
+            throw new InvalidArgumentException(
+                sprintf("Domain name '%s' must be more than 3 symbol", $value)
+            );
+        }
+
+        if (!filter_var($value, FILTER_VALIDATE_DOMAIN, FILTER_FLAG_HOSTNAME) && !preg_match(
+                '/^[a-z0-9.-]+$/',
+                $value
+            )) {
+            throw new DomainException(
                 sprintf("Invalid domain name format: %s", $value)
             );
         }
